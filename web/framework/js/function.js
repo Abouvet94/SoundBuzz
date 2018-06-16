91b58ed4b1a3fbbc4c4da6392a98f844bae6a9db
Abouvet94 <abouvet94@gmail.com>
2018-06-16 16:50:08 +0200

    Menu Dynamique + Commentaires Codes

---
+++ web/framework/js/function.js
@@ -0,0 +1,22 @@
+//Variables
+var menu = document.getElementById('Menu');
+var body = document.getElementById('corp_section');
+var menuli = '#'+menu.id+" li";
+
+//Function Click Menu
+$(menuli).click(function(){  
+    var body_enfant = $("#corp_section").children(); 
+    var id = "#"+$(this).attr('id')+"_pages"; 
+    if (body_enfant){ body_enfant.hide();}
+    $('#corp_section').append(ShowDiv(id));
+})
+
+//Hide div au double click
+$(body).dblclick(function(){
+    var div = $(this).children('div');
+    HideDiv(div);
+});
+
+//Function Div (Hide + Show)
+function HideDiv(id){ setTimeout(function(){ $(id).hide();}, 2500);}
+function ShowDiv(id){ $(id).show(); }
