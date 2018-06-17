905d768d74ec779c645d3a78634780ffa57f0557
Abouvet94 <abouvet94@gmail.com>
2018-06-17 14:39:31 +0200

    Radial music OK

--- web/framework/js/p5/radialsound.js
+++ web/framework/js/p5/radialsound.js
@@ -1,22 +1,22 @@
-var song;
+var song = undefined;
+var flag = false;
 var amp;
 var volhistory = [];
 var barWidth = 20;
 var lastBar = -1;
 var source, resulta;
 
-// var playermusic = document.getElementById('playermusic');
 //Function Pause souris
-// function mousePressed() {
-//   if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-//     song.pause(); // .play() will resume from .pause() position
-//     background(255,0,0);
-//   } else {
-//     song.play();
-//     background(0,255,0);
-//   }
-// }
-
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop(); // .play() will resume from .pause() position
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
+}
+//Récupération de la Source du Son
 $('#playermusic').each(function(){
   playermusic.onclick = function(e) {
     e.preventDefault();
@@ -25,25 +25,13 @@ $('#playermusic').each(function(){
     source = document.getElementById('audioSource');
     source.src = elm.getAttribute('data-value');
     var idframe = source.src.indexOf('framework');
-      if(idframe.value == '')
-        {
-          alert('hello world');
-        }
     if (typeof source.src === 'string' && idframe > 15 ) {
       var result = source.src.substring(idframe);
       resulta = result;
+      setup();
     }
   };
 });
-
-function preload(){
-  if(resulta){
-    song = loadSound("../"+resulta);
-  }else{
-    song = loadSound("../framework/audio/dj_anilson.mp3");
-  }
-}
-
 //function LineOndes
 function LineOndes(volhistory, vol, i, max, mini, maxi){
   var r = map(volhistory[i], vol, max, mini, maxi);
@@ -51,37 +39,66 @@ function LineOndes(volhistory, vol, i, max, mini, maxi){
   var ry = r * sin(i);
   return vertex(rx, ry); 
 }
-
 //Function de lancement
 function setup() {
-    console.log(resulta);
+  if( typeof resulta !== 'undefined'){
+    //Charge le sons
+    if( flag === true ){
+      song.stop();
+    }
+    song = loadSound(
+      "../"+String(resulta), 
+      loaded,
+      null,
+      testSong
+    );
+    //Initialisation du fond
     createCanvas(800, 600);
     angleMode(DEGREES);
-    song.play();
-    amp = new p5.Amplitude();  
+    //Récupération de l'amplitude
+    if ( typeof song !== 'undefined'){
+      amp = new p5.Amplitude();
+    }
+  }
 }
-
+//Function pour load la musique selectionné
+function loaded(){
+  song.load();
+}
+//Test si un sons est en cours, false => lancer Sons, true => up flag
+function testSong(){
+  var SonsPlaying = song.isPlaying();
+  if ( SonsPlaying === false ){
+      song.play();
+  }
+  if ( SonsPlaying === true ){
+    flag = true;
+  }
+}
 //Corp
 function draw() {
-      background("rgb(26, 26, 26)");
-      var vol = amp.getLevel();
-      volhistory.push(vol);
-    
-      translate(width / 2, height / 2);
-      beginShape();
-    
-      //Joue sur la couleur de la ligne
-      stroke(255, 200, 0);
-      strokeWeight(1);
-      noFill();
-    
-      for (var i = 0; i < 360; i++) {
-        LineOndes(volhistory, vol, i, 1.1, 250, 300);
-      }
-    
-      endShape();
-    
-      if (volhistory.length > 360) {
-        volhistory.splice(0, 1);
-      }
+  if ( typeof amp !== 'undefined'){
+    background("rgb(26, 26, 26)");
+    var vol = amp.getLevel();
+    volhistory.push(vol);
+    translate(width / 2, height / 2);
+    beginShape();
+    //Joue sur la couleur de la ligne
+    stroke(255, 200, 0);
+    strokeWeight(1);
+    noFill();
+    for (var i = 0; i < 360; i++) {
+      LineOndes(volhistory, vol, i, 1.1, 250, 300);
     }
+    endShape();
+    if (volhistory.length > 360) {
+      volhistory.splice(0, 1);
+    }
+  }
+}
+//Function pression Clavier
+function keyPressed() {
+  if (keyCode === ENTER ) {
+    song.stop();
+  } 
+}
